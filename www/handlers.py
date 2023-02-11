c90615fd59799701a64839ec4f81e964c21136cf
harold <284610625@qq.com>
2023-02-11 11:09:46 +0800

    编写API

--- www/handlers.py
+++ www/handlers.py
@@ -23,6 +23,19 @@ async def index(request):
     # 解密cookie
 
 
+@get('/api/users')
+def api_get_users(*, page='1'):
+    page_index = get_page_index(page)
+    num = yield from User.findNumber('count(id)')
+    p = Page(num, page_index)
+    if num == 0:
+        return dict(page=p,users=())
+    users = yield from User.findAll(orderBy='created_at desc',limit=(p.offset,p.limit))
+    for u in users:
+        u.passwd = '******'
+    return dict(page=p,users=users)
+
+
 async def cookie2user(cookieStr):
     if not cookieStr:
         return None
